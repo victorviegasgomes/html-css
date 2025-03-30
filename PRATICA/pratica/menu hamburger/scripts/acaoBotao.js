632d28cb5f95aeb978e202194fe9f144fe7ada66
victorviegasgomes <vviegasgomes@gmail.com>
2025-03-30 01:22:59 +0100

    me aprofundando mais em media queries

---
+++ PRATICA/pratica/menu hamburger/scripts/acaoBotao.js
@@ -0,0 +1,9 @@
+function mostrarOcultarMenu(){
+				var menu = document.getElementById('menu')
+				if(menu.style.display == 'block'){
+								menu.style.display = 'none'
+				}else{
+								menu.style.display = 'block'
+				}
+}
+
