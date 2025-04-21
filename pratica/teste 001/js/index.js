c2fd202a525ca6ce2d8d87aa820a08b82c6beef3
victorviegasgomes <vviegasgomes@gmail.com>
2025-04-21 22:13:13 +0100

    repositório actualizado 21/04/2025

---
+++ pratica/teste 001/js/index.js
@@ -0,0 +1,21 @@
+const palavras = [
+  "lorem", "ipsum", "dolor", "sit", "amet", "consectetur",
+  "adipiscing", "elit", "sed", "do", "eiusmod", "tempor",
+  "incididunt", "ut", "labore", "et", "dolore", "magna",
+  "aliqua", "enim", "ad", "minim", "veniam", "quis", "nostrud"
+];
+
+function gerarLorem() {
+  let texto = "";
+  const numPalavras = Math.floor(Math.random() * 30) + 20;
+
+  for (let i = 0; i < numPalavras; i++) {
+    let palavra = palavras[Math.floor(Math.random() * palavras.length)];
+    texto += palavra + " ";
+  }
+
+  texto = texto.charAt(0).toUpperCase() + texto.slice(1).trim() + ".";
+  document.getElementById("saida").innerHTML = `<p>${texto}</p>`;
+}
+
+window.addEventListener("load", gerarLorem)
